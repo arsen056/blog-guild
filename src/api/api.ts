7a9480a733cecccbcf82fca66e89725f42fb82ae
arsen056 <vurdihanov@gmail.com>
2023-03-03 23:20:07 +0300

    feat: add get post by id and posts for blog, api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {BlogType, GetBlogsResponseType} from "features/blogs/types";
-import {GetPostsResponseType} from "features/posts/types";
+import {GetPostsResponseType, PostType} from "features/posts/types";
 
 const instance = axios.create({
   baseURL: 'https://blogs-and-posts-ver2-dgfu.vercel.app/'
@@ -10,10 +10,16 @@ export const API = {
   getBlogs (params: any) {
     return instance.get<GetBlogsResponseType>('blogs', {params} )
   },
+  getBlog(id: string) {
+    return instance.get<BlogType>(`blogs/${id}`)
+  },
+  getPostsForBlog(blogID: string) {
+    return instance.get<PostType[]>(`blogs/${blogID}/posts`)
+  },
   getPosts() {
     return instance.get<GetPostsResponseType>('posts')
   },
-  getBlog(id: string) {
-    return instance.get<BlogType>(`blogs/${id}`)
+  getPost(id: string) {
+    return instance.get<PostType>(`posts/${id}`)
   }
 }
