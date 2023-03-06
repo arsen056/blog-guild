d22d08b2d0bee4894a37a9481e0a9341c4232e49
arsen056 <vurdihanov@gmail.com>
2023-03-06 20:24:46 +0300

    feat: add img for posts, and margin

--- src/common/components/postItem/PostItem.tsx
+++ src/common/components/postItem/PostItem.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import s from './PostItem.module.css'
 import {isoFormatDate} from "common/utils/isoFormatDate";
 import {Link} from "react-router-dom";
+import postImg from 'common/assets/post.png'
 
 type PostItemType = {
   id: string
@@ -22,7 +23,7 @@ export const PostItem: FC<PostItemType> = ({blogName, isLoading, id, date, title
       <Link to={id}>
         <div className={s.card}>
           <div className={s.imgWrapper}>
-            <img src="https://via.placeholder.com/300x180" alt=""/>
+            <img src={postImg} alt="post"/>
           </div>
 
           <div className={s.content}>
